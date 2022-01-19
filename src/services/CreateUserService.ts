c75ad4ef76fa9f8862b1e03011f024c183d88584
wbrunovieira <wbrunovieira77@gmail.com>
2022-01-19 21:23:55 +0000

    create check if the phone exists and hashed the password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,5 @@
 import { getRepository } from 'typeorm';
-//import { hash } from 'bcryptjs';
-
+import { hash } from 'bcryptjs';
 
 import User from '../models/Users';
 
@@ -9,43 +8,41 @@ interface Request {
     email: string;
     phone: string;
     password: string;
-
 }
 
 class CreateUserService {
-    public async execute({ name, email, phone, password }:Request): Promise<User>{
+    public async execute({
+        name,
+        email,
+        phone,
+        password,
+    }: Request): Promise<User> {
         const userRepository = getRepository(User);
 
         const checkUserExists = await userRepository.findOne({
-
             where: { email },
         });
 
-
-
-        if(checkUserExists) {
+        if (checkUserExists) {
             throw new Error('email already exists.');
         }
 
-        // const checkUserPhone = await userRepository.findOne({
-        //     where: { phone },
-        // });
+        const checkUserPhone = await userRepository.findOne({
+            where: { phone },
+        });
 
-        if(checkUserExists) {
+        if (checkUserPhone) {
             throw new Error('phone already exists.');
         }
 
-       // const hashedPassword = await hash(password,8);
+        const hashedPassword = await hash(password, 8);
 
         const user = userRepository.create({
-
             name,
             email,
-           // password: hashedPassword,
-            password,
-            phone
+            password: hashedPassword,
+            phone,
         });
-
 
         await userRepository.save(user);
 
