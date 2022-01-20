b9ac83c289004eca0b365657acff87d726243b74
wbrunovieira <wbrunovieira77@gmail.com>
2022-01-20 08:32:50 +0000

    added the authenticate, creted the route session, AuthemtivcateUserService, to generate the token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from 'express';
 import usersRouter from './users.routes';
+import usersSession from './sessions.routes';
 
 const routes = Router();
 
 
 routes.use('/users', usersRouter);
+routes.use('/session', usersSession);
 
 routes.get('/', (request: Request, response: Response) => {
     response.json({message: "Hello"})
